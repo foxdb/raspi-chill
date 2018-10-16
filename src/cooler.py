e479b56f12929b55d5eb19617687ba8e8bac6cae
Benjamin <ben@codafication.com>
2018-10-16 22:30:08 +1000

    cooler clean up

--- src/cooler.py
+++ src/cooler.py
@@ -8,7 +8,7 @@ config.read(os.path.dirname(os.path.realpath(__file__)) + "/config.ini")
 
 COOLING_GPIO = config.getint('actuators', 'cooling_gpio')
 
-cooling = DigitalOutputDevice(COOLING_GPIO)
+cooling = DigitalOutputDevice(COOLING_GPIO, active_high=False)
 
 # fns: on, off, close
 # params: value
@@ -17,23 +17,14 @@ cooling = DigitalOutputDevice(COOLING_GPIO)
 
 
 def turn_cooling_on():
-    #     if cooling.value == True:
-    cooling.off()
+    if cooling.value == False:
+        cooling.on()
 
 
 def turn_cooling_off():
-    #     if cooling.value == False:
-    cooling.on()
+    if cooling.value == True:
+        cooling.off()
 
 
 def is_cooling_on():
     return cooling.value
-
-
-print 'turning on'
-turn_cooling_off()
-
-sleep(10)
-
-print 'turning off'
-turn_cooling_on()
