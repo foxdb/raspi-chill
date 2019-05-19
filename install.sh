9eca90f5023d2c8fd4830765e2115ed0c13cd105
foxdb <bens.schmitt@gmail.com>
2019-05-19 15:38:45 +1000

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # everything is to debug here, just throwing random cmds without testing
+
 pip install -r requirements.txt
 cp config.example.ini config.ini
 chmod +x src/spindel_server.py
