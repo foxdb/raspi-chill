8af145ebdebfeda26456f90d13adc147fa254c01
ben <1695374+foxdb@users.noreply.github.com>
2023-09-10 18:20:24 +0200

    chore: updated install instructions

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@
 
 pip install -r requirements.txt
 cp config.example.ini config.ini
+# make edits to config.ini
 chmod +x src/spindel_server.py
 chmod +x src/regulate.py
 mkdir src/logs
 
-# edit crontab to add that stuff
+# then "contrab -e" and add:
 
 # */5 * * * * /home/pi/raspi-chill/remote/s3-upload.sh
 # @reboot sleep 5 && nohup sudo python3 /home/pi/raspi-chill/src/regulate.py &
